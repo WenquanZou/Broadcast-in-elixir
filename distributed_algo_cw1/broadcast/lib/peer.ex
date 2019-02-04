4f294d0fe1d9fc7eeb51b92fd0e19d1074a17aef
jyk416 <jyk416@ic.ac.uk>
2019-02-04 18:01:12 +0000

    Implemented simple broadcast, need fixing

--- distributed_algo_cw1/broadcast/lib/peer.ex
+++ distributed_algo_cw1/broadcast/lib/peer.ex
@@ -1,15 +1,19 @@
 defmodule Peer do
 
   def start do
+
+    IO.puts ["Create peer at ", DAC.self_string()]
     receive do 
       {:bind, neighbours} -> next neighbours
     end
   end
 
   defp next neighbours do
+  message = "Hello, world."
     receive do 
       {:broadcast, max_broadcasts, timeout} -> 
-    # TODO
+        for dest <- neighbours, do:
+          IO.puts ["Sending message from ", DAC.self_string()]
     end
   end
 end
--- distributed_algo_cw1/broadcast/mix.exs
+++ distributed_algo_cw1/broadcast/mix.exs
@@ -1,12 +1,12 @@
-defmodule Broadcast.MixProject do
+defmodule Broadcast.Mixfile do
   use Mix.Project
 
   def project do
     [
       app: :broadcast,
       version: "0.1.0",
-      elixir: "~> 1.8",
-      start_permanent: Mix.env() == :prod,
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
@@ -22,7 +22,7 @@ defmodule Broadcast.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
