9e6c1d7511a9cb63db327acc44e1a1523d5e6703
Wenquan Zou <wz1816@ic.ac.uk>
2019-02-05 15:32:00 +0000

    [wz1816]Implement Broadcast system for elixir using flooding algorithms.

--- distributed_algo_cw1/broadcast/lib/broadcast.ex
+++ distributed_algo_cw1/broadcast/lib/broadcast.ex
@@ -1,15 +1,19 @@
 defmodule Broadcast do
   def main do
-    no_peers = 0..(5 - 1)
-    max_broadcasts = 1000
+    no_peers = hd(DAC.int_args())
+    max_broadcasts = 10_000_000
     timeout = 3000
+
+    IO.puts ["Broadcasting at ", DAC.self_string()]
+
     # Create peers
-    peers = for i <- no_peers, do:
+    peers = for i <- 0..(no_peers - 1), do:
       DAC.node_spawn("", 1, Peer, :start, [])
 
     #Binding every peer with its neighbours
     for peer <- peers, do:
       send peer, {:bind, peers}
+
     for peer <- peers, do:
       send peer, { :broadcast, max_broadcasts, timeout }
   end
