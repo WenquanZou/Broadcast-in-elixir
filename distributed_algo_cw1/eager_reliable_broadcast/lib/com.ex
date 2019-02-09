893b7a1456f976b0d4f34da5b9ebbd14f053543f
jyk416 <jyk416@ic.ac.uk>
2019-02-09 15:30:24 +0000

    Refactor eager reliable broadcast

--- distributed_algo_cw1/eager_reliable_broadcast/lib/com.ex
+++ distributed_algo_cw1/eager_reliable_broadcast/lib/com.ex
@@ -1,7 +1,6 @@
 defmodule Com do
 
   def start(processes) do
-
     receive do
       {:bind, rb, pid_peer} ->
         next(rb, processes, pid_peer)
@@ -12,34 +11,36 @@ defmodule Com do
   messages = Enum.reduce processes, %{}, fn(peer, acc) -> Map.put(acc, peer, {0, 0}) end
     receive do
       {:rb_deliver, _, {:rb_broadcast, max_broadcasts, timeout}} ->
-        listen(messages, my_rb, 0, max_broadcasts, timeout, pid_peer)
+        Process.send_after(self(), {:timeout}, timeout)
+        listen(messages, my_rb, max_broadcasts, pid_peer)
     end
   end
 
-  defp listen(messages, my_rb, timeout, max_broadcasts, max_timeout, pid_peer) do
+  defp broadcast(my_rb, messages) do
+    send my_rb, {:rb_broadcast, {:message}}
+    messages = Enum.reduce Map.keys(messages), messages,
+    fn(peer, acc) ->
+      {sent, received} = messages[peer];
+      Map.put(acc, peer, { sent+1, received})
+    end
+    messages
+  end
+
+  defp listen(messages, my_rb, max_broadcasts, pid_peer) do
     receive do
       {:rb_deliver, from, _} ->
         {sent, received} = messages[from]
-        listen(Map.put(messages, from, {sent, received+1}), my_rb, timeout, max_broadcasts, max_timeout, pid_peer)
+        listen(Map.put(messages, from, {sent, received+1}), my_rb, max_broadcasts, pid_peer)
+      {:timeout} -> IO.puts "#{inspect pid_peer}: #{for p <- messages, do: ({_, t} = p; inspect t)}"
     after
-      timeout ->
-        if sent(messages) < max_broadcasts do
-          send my_rb, {:rb_broadcast, {:message}}
-          messages = Enum.reduce Map.keys(messages), messages,
-          fn(peer, acc) ->
-            {sent, received} = messages[peer];
-            Map.put(acc, peer, { sent+1, received})
-          end
-          timeout = if sent(messages) == max_broadcasts, do:
-          max_timeout, else: timeout
-          listen(messages, my_rb, timeout, max_broadcasts, max_timeout, pid_peer)
-        else
-        IO.puts "#{inspect pid_peer}: #{for p <- messages, do: ({_, t} = p; inspect t)}"
-        end
-      end
-  end
-    defp sent(messages) do
-      {sent, _} = hd(Map.values(messages))
-      sent
+      0 ->
+        messages = if sent(messages) < max_broadcasts, do: broadcast(my_rb, messages), else: messages
+        listen(messages, my_rb, max_broadcasts, pid_peer)
     end
+  end
+
+  defp sent(messages) do
+    {sent, _} = hd(Map.values(messages))
+    sent
+  end
 end
