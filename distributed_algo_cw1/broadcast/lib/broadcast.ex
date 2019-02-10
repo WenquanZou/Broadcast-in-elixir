6a91f5892cf8b8ebc0027ca1be9f136cdfbf5f63
jyk416 <jyk416@ic.ac.uk>
2019-02-10 12:52:02 +0000

    Docker add in

--- distributed_algo_cw1/broadcast/lib/broadcast.ex
+++ distributed_algo_cw1/broadcast/lib/broadcast.ex
@@ -1,6 +1,25 @@
 defmodule Broadcast do
   def main do
     no_peers = hd(DAC.int_args())
+    max_broadcasts = 1000
+    timeout = 3000
+
+    IO.puts ["Broadcasting at ", DAC.self_string()]
+
+    # Create peers
+    peers = for i <- 0..(no_peers - 1), do:
+      DAC.node_spawn("", 1, Peer, :start, [])
+
+    #Binding every peer with its neighbours
+    for peer <- peers, do:
+      send peer, {:bind, peers}
+
+    for peer <- peers, do:
+      send peer, { :broadcast, max_broadcasts, timeout }
+  end
+
+  def main_net do
+    no_peers = hd(DAC.int_args())
     max_broadcasts = 10_000_000
     timeout = 3000
 
